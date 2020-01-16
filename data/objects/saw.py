6a9f5129d336bc4a81c6b64b34fb0dafad0f9d00
Misantrhope <alexmoro112@mail.ru>
2020-01-16 15:55:55 +0300

    Изменён хитбокс.

--- data/objects/saw.py
+++ data/objects/saw.py
@@ -2,7 +2,6 @@ import pygame
 import os
 
 
-
 class Saw():
     img = [pygame.image.load(os.path.join('data\images', 'SAW0.png')),
            pygame.image.load(os.path.join('data\images', 'SAW1.png')),
@@ -22,9 +21,10 @@ class Saw():
         self.hitbox = (self.x + 5, self.y + 5, self.width - 12, self.height - 5)
         if self.count >= 8:
             self.count = 0
-        win.blit(pygame.transform.scale(self.img[self.count // 2], (64, 64)), (self.x, self.y))
+        win.blit(pygame.transform.scale(self.img[self.count // 2], (48, 48)), (self.x, self.y))
         self.count += 1
-        #pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+        # pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+
     def collide(self, rect):
         # Проверяем если x координата игрока сталкиваеться с чем либо.
         if rect[0] + rect[2] > self.hitbox[0] and rect[0] < self.hitbox[0] + self.hitbox[2]:
